08e144bd9e2a01640ad7491ed7dfec240534beb5
Pedro Henrique <pedro00dk@gmail.com>
2019-07-06 19:22:34 -0300

    Add mul operation on update

--- src/updates.js
+++ src/updates.js
@@ -4,7 +4,10 @@ db = db.getSiblingDB('carservice')
 // Adicionar endereço de uma pessoa
 const name = 'rita da conceição'
 // before
-db.people.find({ $text: { $search: `"${name}"` } })
-db.people.update({ $text: { $search: `"${name}"` } }, { $set: { 'address.state': 'pernambuco' } })
+db.people.find({ name })
+db.people.updateOne({ name }, { $set: { 'address.state': 'pernambuco' } })
 // after
-db.people.find({ $text: { $search: `"${name}"` } })
+db.people.find({ name })
+
+// corrigir valor estimado dos das corridas com valores estimados maior que 20 atuais em 5%
+db.trips.update({ estimatedValue: { $gt: 20 } }, { $mul: { estimatedPrice: 1.05 } })
